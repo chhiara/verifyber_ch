e2caf3a2524a9640a2794a54ae0365647f6683ac
chhiara <1234chiaa@gmail.com>
2025-01-27 16:20:39 +0100

    small changes

--- tests_ch/tests_Loadtrk.py
+++ tests_ch/tests_Loadtrk.py
@@ -1,9 +1,15 @@
 import numpy as np
+import sys
+import os
+
+repo_path=os.environ["HOME"] + f"/local/verifyber_ch/"
+sys.path.append(repo_path)
 
 from utils.data.selective_loader_numba import load_streamlines as load_streamlines_fast
 
 
-T_file="/raid/home/nilab/chiara/datasets/test_verifyber/trk_data/sub-1112__FAT_L__affined_tract.trk"
+T_file="/home/chiara/mnt/hd14TB_alberto/chhiara/TractoInferno_Nilab_TractoAnomaly/derivatives/register_to_MNI_affine/sub-1112/sub-1112__FAT_L__affined_tract.trk"
+
 li_idx=[0,4,5]
 #Q: what is lennghts?
 streams, lengths = load_streamlines_fast(T_file,
