36c2aa8405c66703f53e3f7aab09cdf07fb84dd7
chhiara <1234chiaa@gmail.com>
2025-02-27 10:30:59 +0100

    added config file and code to perform test of the whole perilesional area for ilf@

--- checkpoints/tr_anomaly/ilf_l/sdec_tr_anomaly_ilf/cmd_run.sh
+++ checkpoints/tr_anomaly/ilf_l/sdec_tr_anomaly_ilf/cmd_run.sh
@@ -9,23 +9,32 @@ path_code_parent="/app/src/local/"
 #path_code_parent="/home/chiara/local/"
 
 repo_verifyber_path="${path_code_parent}/verifyber_ch/"
-experiment_path="${repo_verifyber_path}/checkpoints/sdec_tr_anomaly_ilf/"
+experiment_path="${repo_verifyber_path}/checkpoints/tr_anomaly/ilf_l/sdec_tr_anomaly_ilf/"
 
 log_file_train="${experiment_path}/log_file_train.txt"
 log_file_test="${experiment_path}/log_file_test.txt"
+log_file_test_allTum="${experiment_path}/log_file_test_AllTumArea.txt"
+
 
 
 cd $repo_verifyber_path
 
-
+#-----train and test with balanced samples: 40% positive, 30% nn negative, 30% random negative
 #Train
 #python main.py TR-ANOMALY -opt train --exp ${experiment_path}\
 #              --with_gt &> $log_file_train 
 
 
 #Test
-best_model_path="${repo_verifyber_path}/runs/sdec_nodropout_loss_nll-tracto-anomaly-ilf-l_1/models/best_model_ep-300_score-0.778140.pth"
-python main.py TR-ANOMALY -opt test --exp ${experiment_path}\
-              --with_gt  --weights ${best_model_path} --save_pred &> $log_file_test 
+#best_model_path="${repo_verifyber_path}/runs/sdec_nodropout_loss_nll-tracto-anomaly-ilf-l_1/models/best_model_ep-300_score-0.778140.pth"
+#python main.py TR-ANOMALY -opt test --exp ${experiment_path}\
+#              --with_gt  --weights ${best_model_path} --save_pred &> $log_file_test 
               
 
+#Test on all the streamline in the perilesional area
+best_model_path="${repo_verifyber_path}/runs/sdec_nodropout_loss_nll-tracto-anomaly-ilf-l_1/models/best_model_ep-300_score-0.778140.pth"
+config_allTum="${experiment_path}/config_testAllTumArea.txt"
+
+python main.py TR-ANOMALY -opt test --exp ${experiment_path} --config ${config_allTum}\
+              --with_gt  --weights ${best_model_path} --save_pred &> $log_file_test_allTum 
+              
