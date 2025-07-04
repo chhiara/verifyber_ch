981f4557c3dc3a0db655274a20cee5945f4f4ab9
chhiara <1234chiaa@gmail.com>
2025-07-04 10:26:42 +0200

    added testing on apss

--- checkpoints/tr_anomaly/ilf_l/sdec_tr_anomaly_ilf/cmd_run.sh
+++ checkpoints/tr_anomaly/ilf_l/sdec_tr_anomaly_ilf/cmd_run.sh
@@ -1,7 +1,7 @@
 
 
 #!/usr/bin/bash 
-#docker run -v /raid/home/nilab/chiara/datasets/input_model/:/app/data/input_model/ -v /raid/home/nilab/chiara/local/:/app/src/local/  --rm -it --gpus device=3 verifyber --shm-size=256m
+#docker run -v /raid/home/nilab/chiara/datasets/input_model/:/app/data/input_model/ -v /raid/home/nilab/chiara/local/:/app/src/local/  --rm -it --gpus device=0 verifyber --shm-size=800m
 #conda run --no-capture-output -n verifyber bash cmd_run.sh
 
 path_code_parent="/app/src/local/"
@@ -15,7 +15,7 @@ experiment_path="${repo_verifyber_path}/checkpoints/tr_anomaly/ilf_l/sdec_tr_ano
 log_file_train="${experiment_path}/log_file_train.txt"
 log_file_test="${experiment_path}/log_file_test.txt"
 log_file_test_allTum="${experiment_path}/log_file_test_AllTumArea.txt"
-
+log_file_test_apss="${experiment_path}/log_file_test_Apss21.txt"
 
 
 cd $repo_verifyber_path
@@ -33,9 +33,20 @@ cd $repo_verifyber_path
               
 
 #Test on all the streamline in the perilesional area
-best_model_path="${repo_verifyber_path}/runs/sdec_nodropout_loss_nll-tracto-anomaly-ilf-l_1/models/best_model_ep-300_score-0.778140.pth"
-config_allTum="${experiment_path}/config_testAllTumArea.txt"
+#best_model_path="${repo_verifyber_path}/runs/sdec_nodropout_loss_nll-tracto-anomaly-ilf-l_1/models/best_model_ep-300_score-0.778140.pth"
+#config_allTum="${experiment_path}/config_testAllTumArea.txt"
 
-python main.py TR-ANOMALY -opt test --exp ${experiment_path} --config ${config_allTum}\
-              --with_gt  --weights ${best_model_path} --save_pred &> $log_file_test_allTum 
+#python main.py TR-ANOMALY -opt test --exp ${experiment_path} --config ${config_allTum}\
+#              --with_gt  --weights ${best_model_path} --save_pred &> $log_file_test_allTum 
               
+
+            
+
+#Test on apss subjects in all streamlines of left hemisphere
+best_model_path="${repo_verifyber_path}/runs/sdec_nodropout_loss_nll-tracto-anomaly-ilf-l_1/models/best_model_ep-300_score-0.778140.pth"
+config_apss="${experiment_path}/config_testApss21.txt"
+
+python main.py TR-ANOMALY -opt test --exp ${experiment_path} --config ${config_apss}\
+              --with_gt  --weights ${best_model_path} --save_pred &> $log_file_test_apss
+              
+
