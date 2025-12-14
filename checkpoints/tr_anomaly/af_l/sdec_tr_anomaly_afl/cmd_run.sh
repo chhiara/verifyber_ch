513be3c0ecffcef88e0bca3bbab35dc1983bf92d
chhiara <1234chiaa@gmail.com>
2025-12-14 13:58:05 +0100

    added test of afl dti

--- checkpoints/tr_anomaly/af_l/sdec_tr_anomaly_afl/cmd_run.sh
+++ checkpoints/tr_anomaly/af_l/sdec_tr_anomaly_afl/cmd_run.sh
@@ -1,7 +1,7 @@
 
 
 #!/usr/bin/bash 
-#docker run -v /raid/home/nilab/chiara/datasets/input_model/:/app/data/input_model/ -v /raid/home/nilab/chiara/local/:/app/src/local/  --rm -it --gpus device=1  --shm-size=800m verifyber:gpu
+#docker run -v /raid/home/nilab/chiara/datasets/input_model/:/app/data/input_model/ -v /raid/home/nilab/chiara/local/:/app/src/local/  --rm -it --gpus device=0  --shm-size=800m verifyber:latest
 #conda run --no-capture-output -n verifyber bash cmd_run.sh
 
 path_code_parent="/app/src/local/"
@@ -18,6 +18,7 @@ log_file_test_allTum="${experiment_path}/log_file_test_AllTumArea.txt"
 log_file_test_apss="${experiment_path}/log_file_test_Apss21.txt"
 log_file_test_apssFiltTumConv="${experiment_path}/log_file_test_Apss21_FiltTum_TumCCConv.txt"
 log_file_test_apssFiltTumConv_csd="${experiment_path}/log_file_test_ApssExt_FiltTum_TumCCConv_csd_afl.txt"
+log_file_test_apssFiltTumConv_dti="${experiment_path}/log_file_test_ApssExt_FiltTum_TumCCConv_dti_afl.txt"
 
 
 
@@ -69,13 +70,20 @@ cd $repo_verifyber_path
 
 
 
-#Test on apss subjects in the perilesional area of tum I estimated is moved for at least 5 mm; the tum used are not the original ones but are corrected to
+#Test on apss Ext subjects in the perilesional area of tum I estimated is moved for at least 5 mm; the tum used are not the original ones but are corrected to
 #to be convex and connected  
 #---test on AF_L  CSD TRACTO
-best_model_path="${repo_verifyber_path}/runs/sdec_nodropout_loss_nll-tracto-anomaly-af-l_0/models/best_model_ep-420_score-0.816775.pth"
-config_apss="${experiment_path}/config_testApssExt_csd_AF_L.txt"
+#best_model_path="${repo_verifyber_path}/runs/sdec_nodropout_loss_nll-tracto-anomaly-af-l_0/models/best_model_ep-420_score-0.816775.pth"
 
+#config_apss="${experiment_path}/config_testApssExt_csd_AF_L.txt"
+#python main.py TR-ANOMALY-SINGLE-SUBID -opt test --exp ${experiment_path} --config ${config_apss}\
+#              --with_gt  --weights ${best_model_path} --save_pred &> $log_file_test_apssFiltTumConv
+       
+
+#---test on AF_L  DTI TRACTO
+best_model_path="${repo_verifyber_path}/runs/sdec_nodropout_loss_nll-tracto-anomaly-af-l_0/models/best_model_ep-420_score-0.816775.pth"
+config_apss="${experiment_path}/config_testApssExt_dti_AF_L.txt"
 python main.py TR-ANOMALY-SINGLE-SUBID -opt test --exp ${experiment_path} --config ${config_apss}\
-              --with_gt  --weights ${best_model_path} --save_pred &> $log_file_test_apssFiltTumConv
+              --with_gt  --weights ${best_model_path} --save_pred &> $log_file_test_apssFiltTumConv_dti
        
        
